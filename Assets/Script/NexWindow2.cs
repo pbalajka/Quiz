8976e981c4e01b7d9127d4eec2f6adfd9613ab4b
pbalajka <balajka.pavol@centrum.sk>
2016-03-08 15:24:48 +0100

    Sript next windows

---
+++ Assets/Script/NexWindow2.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NexWindow2 : MonoBehaviour {
+	private bool isAndroid;
+	// Use this for initialization
+	void Start () {
+		if (Application.platform == RuntimePlatform.Android) {
+			isAndroid = true;
+		} else {
+			isAndroid = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isAndroid) {
+			if (Input.touches.Length > 0 && Input.GetTouch (0).phase == TouchPhase.Began) {
+				SceneManager.LoadScene ("game1", LoadSceneMode.Single);
+			}
+		} else {
+			if(Input.GetMouseButtonDown (0)) {
+				SceneManager.LoadScene ("game1", LoadSceneMode.Single);
+			}
+		}
+	
+	}
+}
---
+++ Assets/Script/NextWindow.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class NextWindow : MonoBehaviour {
+	private bool isAndroid;
+
+	// Use this for initialization
+	void Start () {
+		if (Application.platform == RuntimePlatform.Android) {
+			isAndroid = true;
+		} else {
+			isAndroid = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isAndroid) {
+			if (Input.touches.Length > 0 && Input.GetTouch (0).phase == TouchPhase.Began) {
+				SceneManager.LoadScene ("screen2", LoadSceneMode.Single);
+			}
+		} else {
+			if(Input.GetMouseButtonDown (0)) {
+				SceneManager.LoadScene ("screen2", LoadSceneMode.Single);
+			}
+		}
+	}
+}
