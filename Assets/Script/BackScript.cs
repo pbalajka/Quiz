6faf24acfb2626ec4293569d40e3fdb66bff678a
dbaranec <dbaranec@163-d4.dorm.umb.sk>
2016-03-11 11:20:50 +0100

    pridanieback tlacitka

---
+++ Assets/Script/BackScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class BackScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void BackScene(){
+		SceneManager.LoadScene ("game1", LoadSceneMode.Single);
+	}
+}
