125a5e3277a5bdd1e857a953fde71d26346caad1
pbalajka <balajka.pavol@centrum.sk>
2016-03-12 20:52:47 +0100

    Automatic next question and question 2

--- Assets/Script/HideShowKeyboard.cs
+++ Assets/Script/HideShowKeyboard.cs
@@ -1,21 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class HideShowKeyboard : MonoBehaviour {
 	private TouchScreenKeyboard keyboard;
 	private bool ok = false;
 
 	public void ShowKeyboard() {
-		keyboard = TouchScreenKeyboard.Open ("Input char", TouchScreenKeyboardType.Default, false, false, false, false);
-		ok = true;
+		/*keyboard = TouchScreenKeyboard.Open ("Input char", TouchScreenKeyboardType.Default, false, false, false, false);
+		ok = true;*/
+		NextScene ();
 	}
 
-     void OnGUI() {		
+    /* void OnGUI() {		
 		if (keyboard != null && keyboard.done && ok) {
 			ok = false;
 			string text = keyboard.text;
 			char[] textArray = text.ToCharArray();
 			SetTextScript.SetText(textArray[0]);
 		}
+	}*/
+
+	void NextScene() {
+		string question = "question_";
+		int actualScene = ActualSceneNunberScript.SceneNumber();
+		actualScene = ++actualScene;
+		string scene = question + actualScene.ToString ();
+		SceneManager.LoadScene (scene, LoadSceneMode.Single);
 	}
 }
