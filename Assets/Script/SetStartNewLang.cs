e49126d59c1ea7c6d00ee4960f1726e7d878e227
dbaranec <dbaranec@MacBook-Pro.local>
2016-03-12 19:13:15 +0100

    pridanie scriptov na menu obrazovku

---
+++ Assets/Script/SetStartNewLang.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class SetStartNewLang : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Text> ().text = LanguageLabelScript.GetNewGame ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
