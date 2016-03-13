e79a8a9650c03424ecbe92f44733cd15dc41629f
dbaranec <dbaranec@MacBook-Pro.local>
2016-03-13 19:20:03 +0100

    roll disable

--- Assets/Script/Disablebutton.cs
+++ Assets/Script/Disablebutton.cs
@@ -5,19 +5,27 @@ using System.Collections;
 public class Disablebutton : MonoBehaviour {
 	Text text;
 	Button butt;
+	public Scrollbar scrollbar;
+ 
 
-	void Start(){
-		Invoke ("ChangeTextToTransparent", 5);
+	void Update(){
+		ChangeTextToTransparent ();
+
+
 	}
 
 
 	//zmeni farbu textu
 	public void ChangeTextToTransparent(){
 		
+		if (scrollbar.value == 0f) {
 			text =	gameObject.GetComponentsInChildren<Text> () [0];
 			butt = GetComponent<Button> ();
 			butt.interactable = true;
 			text.color = new Color32 (1, 16, 255, 255);
+		}
+
+
 
 
 	}
