ff76d0e694b8ee1c3ddb96daf311e74d92340255
dbaranec <dbaranec@MacBook-Pro.local>
2016-04-05 15:58:25 +0200

    SetDescription

---
+++ Assets/Script/SetDescriptionText.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class SetDescriptionText : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Text> ().text = LanguageDescriptionScript.GetDescription ();
+	}
+	
+
+}
