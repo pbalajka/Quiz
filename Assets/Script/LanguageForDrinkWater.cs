2664843a50f67377d2cd1d63450b02feb1c913ec
pbalajka <balajka.pavol@centrum.sk>
2016-04-25 13:59:47 +0200

    pridanie skirptu pre napisi napi sa vody - LanguageForDrinkWater

---
+++ Assets/Script/LanguageForDrinkWater.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LanguageForDrinkWater : MonoBehaviour {
+	private static string[] drinkENG = {"", ""};
+	private static string[] drinkIT = {"", ""};
+	private static string[] drinkESP = {"", ""};
+	private static string[] drinkPT = {"", ""};
+	private static string[] drinkRU = {"", ""};
+
+	private static string errorENG = "Error";
+
+	public static string GetGoodAnswer(int drinkText) {
+		switch (EnumLanguagesScript.GetLanguage ()) {
+		case EnumLanguagesScript.Language.ENG:
+			return drinkENG [drinkText]; 
+		case EnumLanguagesScript.Language.IT:
+			return drinkIT [drinkText];
+		case EnumLanguagesScript.Language.ESP:
+			return drinkESP [drinkText];
+		case EnumLanguagesScript.Language.PT:
+			return drinkPT [drinkText];
+		case EnumLanguagesScript.Language.RU:
+			return drinkRU [drinkText];
+		default :
+			return errorENG;
+		}
+	}
+
+
+	public static int GetCountDrinkText() {
+		return drinkENG.Length;
+	}
+}
