7dd04d8bcd2ade89bb4fd049d5bd3c4999a40b47
dbaranec <dominik.baranec@studenti.umb.sk>
2016-04-27 20:50:53 +0200

    GamaName in description add

---
+++ Assets/Script/LanguageForDescriptionGameName.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LanguageForDescriptionGameName : MonoBehaviour {
+
+	private static string gameNameENG = "The game-quiz  description";
+	private static string gameNameIT = "Descrizione del gioco";
+	private static string gameNameESP = "Descripción del juego";
+	private static string gameNamePT = "Jogo Descrição";
+	private static string gameNameRU = "Описание игры";
+
+	private static string errorENG = "Error";
+
+	public static string GetGameName() {
+		switch (EnumLanguagesScript.GetLanguage()) {
+		case EnumLanguagesScript.Language.ENG:
+			return gameNameENG; 
+		case EnumLanguagesScript.Language.IT:
+			return gameNameIT;
+		case EnumLanguagesScript.Language.ESP:
+			return gameNameESP;
+		case EnumLanguagesScript.Language.PT:
+			return gameNamePT;
+		case EnumLanguagesScript.Language.RU:
+			return gameNameRU;
+		default :
+			return errorENG;
+		}
+	}
+}
---
+++ Assets/Script/SetDescriptionGameName.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+public class SetDescriptionGameName : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Text> ().text = LanguageForDescriptionGameName.GetGameName();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
